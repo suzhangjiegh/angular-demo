fef7731c3d2a254d9bf0ef97a182f92706f8a27d
suzhangjiegh <su2010yx@163.com>
2017-12-06 17:35:45 +0800

    5 服务

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,34 +1,40 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 import {Hero} from "./bean/hero";
-
-
-
-const HEROES: Hero[] = [
-	{id: 11, name: 'Mr. Nice'},
-	{id: 12, name: 'Narco'},
-	{id: 13, name: 'Bombasto'},
-	{id: 14, name: 'Celeritas'},
-	{id: 15, name: 'Magneta'},
-	{id: 16, name: 'RubberMan'},
-	{id: 17, name: 'Dynama'},
-	{id: 18, name: 'Dr IQ'},
-	{id: 19, name: 'Magma'},
-	{id: 20, name: 'Tornado'}
-];
-
+import { HeroService } from "./service/hero.service"
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css'],
+	// providers数组告诉 Angular，当它创建新的AppComponent组件时，也要创建一个HeroService的新实例
+	providers: [HeroService]
 })
-export class AppComponent {
-  	title = 'app';
+export class AppComponent implements OnInit{
 
-	heroes = HEROES;
+	private title : String;
 
-	selectedHero: Hero;
+	private heroes : Hero[];
+
+	private selectedHero: Hero;
+
+	constructor(private heroService: HeroService) {
+
+	}
+
+	ngOnInit(): void {
+		this.title='app';
+		this.getHeroes();
+	}
+
+
+	getHeroes(): void {
+		let that =this;
+		this.heroService.getHeroesSlowly().then(function (heros) {
+			console.log("getHeroes");
+			that.heroes=heros
+		});
+	}
 
 	onSelect(hero: Hero): void {
 
--- src/app/service/hero.service.ts
+++ src/app/service/hero.service.ts
@@ -1,6 +1,21 @@
 import { Injectable } from '@angular/core'
+import {HEROES} from './mock-heroes'
+import {Hero} from "../bean/hero";
+
 
 @Injectable()
 export class HeroService{
 
+	getHeroes(): Promise<Hero[]>{
+
+		console.log('hero.service1',HEROES);
+		return Promise.resolve(HEROES);
+	}
+
+	getHeroesSlowly(): Promise<Hero[]> {
+		return new Promise(resolve => {
+			// Simulate server latency with 2 second delay
+			setTimeout(() => resolve(HEROES), 2000);
+		});
+	}
 }
